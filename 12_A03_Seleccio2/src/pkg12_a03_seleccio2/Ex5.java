c22795254569e50f26f46bb5bcb9f9b6ed4537ea
Kyle Muñoz Andrades <lace22@localhost.localdomain>
2021-01-15 18:16:23 +0100

    fds

--- 12_A03_Seleccio2/src/pkg12_a03_seleccio2/Ex5.java
+++ 12_A03_Seleccio2/src/pkg12_a03_seleccio2/Ex5.java
@@ -5,29 +5,86 @@
  */
 package pkg12_a03_seleccio2;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Calendar;
-
-
-
+import java.util.Scanner;
 
 /**
  *
  * @author lace22
  */
 public class Ex5 {
+
+    /*
+    public static boolean isLeap(int x) {
+        if ((x % 4 == 0 && x % 100 != 0) || (x % 100 == 0 && x % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }*/
     public static void main(String[] args) {
-        int dia, mes, any;
-        int[] intArray = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,0};
+        int dia, mes, any, dias, segundos;
+        Instant instant;
+        long timeStampSeconds;
+        Scanner scanner = new Scanner(System.in);
+        //int[] intArray = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0};
+
         Calendar dataActual = Calendar.getInstance();
+        instant = Instant.now();
+        timeStampSeconds = instant.getEpochSecond();
+
+        System.out.println("Introduce los segundos");
+        segundos = scanner.nextInt();
+        DateFormat horaas = new SimpleDateFormat("HH:mm:ss");
+        DateFormat estampa = new SimpleDateFormat("dd/MM/yyyy");
+        String horaFormato = horaas.format((timeStampSeconds + segundos) * 1000L);
+        String estampaFormato = estampa.format((timeStampSeconds + segundos) * 1000L);
+        System.out.println("De aqui a " + segundos + " segundos serán las " + horaFormato + " del " + estampaFormato);
 
         dia = dataActual.get(Calendar.DAY_OF_MONTH);
-        mes = dataActual.get(Calendar.MONTH)+1;
-        if (dia == intArray[mes]){
-            mes++;
-            dia=0;
+        mes = dataActual.get(Calendar.MONTH) + 1;
+        any = dataActual.get(Calendar.YEAR);
+        //dia = 32;
+        //mes = 12;
+        //any = 2020;
+        switch (mes) {
+            case 2:
+                if ((any % 4 == 0 && any % 100 != 0) || (any % 100 == 0 && any
+                        % 400 == 0)) {
+                    dias = 29;
+                } else {
+                    dias = 28;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                dias = 30;
+                break;
+            default:
+                dias = 31;
         }
-        any = dataActual.get(Calendar.YEAR);  
-        System.out.println(dia+1+"/"+mes+"/"+any);
+        if (dia > dias) {
+            System.out.println("El dia " + dia + " del " + mes + " no existe");
+            return;
+        } else if (dia == dias && mes == 12) {
+            mes = 1;
+            dia = 1;
+            any++;
+        } else if (dia == dias) {
+            mes++;
+            dia = 1;
+        } else {
+            dia++;
+        }
+        String fecha = String.format("%02d/%02d/%04d ", dia, mes, any);
+
+        System.out.println("Mañana es: " + fecha);
+
     }
 
 }
--- 12_A03_Seleccio2/src/pkg12_a03_seleccio2/Ex6.java
+++ 12_A03_Seleccio2/src/pkg12_a03_seleccio2/Ex6.java
@@ -5,11 +5,8 @@
  */
 package pkg12_a03_seleccio2;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.Date;
+import java.time.MonthDay;
+
 import java.util.Scanner;
 
 /**
@@ -17,39 +14,66 @@ import java.util.Scanner;
  * @author lace22
  */
 public class Ex6 {
+
     public static void main(String[] args) {
-      String fecha;
-      Scanner scanner = new Scanner(System.in);
-         System.out.println("Introduce una data en format dd/mm/yyyy");
+        String fecha;
+        Scanner scanner = new Scanner(System.in);
+        MonthDay[] signoszodiaco
+                = {MonthDay.of(12, 22), MonthDay.of(1, 20) //Capri
+                    ,
+                     MonthDay.of(1, 21), MonthDay.of(2, 19) //Aquari
+                    ,
+                     MonthDay.of(2, 20), MonthDay.of(3, 20) //Pisces
+                    ,
+                     MonthDay.of(3, 21), MonthDay.of(4, 19) //Aries
+                    ,
+                     MonthDay.of(4, 20), MonthDay.of(5, 20) //Taure
+                    ,
+                     MonthDay.of(5, 21), MonthDay.of(6, 21) //Geminis
+                    ,
+                     MonthDay.of(6, 22), MonthDay.of(7, 21)//Cancer
+                    ,
+                     MonthDay.of(7, 22), MonthDay.of(8, 21) //Lleo
+                    ,
+                     MonthDay.of(8, 22), MonthDay.of(9, 22) //Verge
+                    ,
+                     MonthDay.of(9, 23), MonthDay.of(10, 22) //Balanca
+                    ,
+                     MonthDay.of(10, 23), MonthDay.of(11, 21) //Escorpio
+                    ,
+                     MonthDay.of(11, 22), MonthDay.of(12, 21)}; //Sagitari
+        String[] nombresStrings = {"Capricorn", "Capricorn", "Aquari", "Aquari",
+            "Aquari", "Pisces", "Pisces", "Aries", "Aries", "Taure", "Taure",
+            "Geminis", "Geminis", "Cancer", "Cancer", "Lleo", "Lleo", "Verge", "Verge",
+            "Balanca", "Balanca", "Escorpio", "Escorpio", "Sagitari", "Sagitari"};
+
+        System.out.println("Introduce una data en format dd/mm/yyyy");
         fecha = scanner.nextLine();
-        if (fecha.length()!= 10){
+        if (fecha.length() != 10) {
             System.out.println("El formato de fecha tiene que ser dd/mm/yyyy");
-            return;      
+            return;
         }
-        String dayInput = fecha.substring(0,2);
-        String monthInput = fecha.substring(3,5);
-        String yearInput = fecha.substring(6,10);
-int y = Integer.parseInt( yearInput );
-int m = Integer.parseInt( monthInput );  // 1-12 for January-December.
-int d = Integer.parseInt( dayInput );
+        String dayInput = fecha.substring(0, 2);
+        String monthInput = fecha.substring(3, 5);
+        String yearInput = fecha.substring(6, 10);
+        int y = Integer.parseInt(yearInput);
+        int m = Integer.parseInt(monthInput);  
+        int d = Integer.parseInt(dayInput);
 
-LocalDate ld = LocalDate.of( y , m , d );
-LocalDate ld2 = LocalDate.of(1999, 03, 13);
-System.out.println(ld.compareTo(ld2));
-if (ld.compareTo(ld2) > 0){
-    System.out.println("Date1 is after Date2");
+     
+        MonthDay ld = MonthDay.of(m, d);
+        for (int i = 0; i < 24;) {
+            boolean is1After2 = signoszodiaco[i].isAfter(ld);
+            boolean is2Before3 = ld.isBefore(signoszodiaco[i + 1]);
+            boolean is2Between1And3 = is1After2 & is2Before3;
+            if (is2Between1And3) {
+                System.out.println("El teu signe es: " + nombresStrings[i]);
+                break;
+            }
+            i = i + 2;
 
-} else if (ld.compareTo(ld2) < 0){
-    System.out.println("Date1 is before Date2");
-    System.out.println(ld.compareTo(ld2));
+        }
 
-}
-else if (ld.compareTo(ld2) == 0){
-    System.out.println("Date1 is before Date2");
-
-}
-
-        System.out.println(ld);
     }
-    
+
 }
